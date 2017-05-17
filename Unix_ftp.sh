a17fd66f9a64fa7376431a32f06a807d3d2e4138
Aamir Shaikh <aamir.shaikh@networkedinsights.com>
2017-05-17 09:07:40 -0400

    seperated ftp script for Linux & Unix(Mac Users)

---
+++ Unix_ftp.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+###   Connecting to remote server via LFTP protocol & downloading "VDOPIA_SDK" directory to the users home directory.
+###   Please make sure to install lftp package before running this script.
+###   You can rename the TARGETFOLDER on line no. 10 if the target folder has changed. Similarly you can also change HOST,USER,PASSWORD,TARGET & LOCAL folder.
+connect_remote_FTP () {
+        echo "Connecting to remote host 34.203.2.241 via LFTP..."
+        sleep 2;
+        HOST='34.203.2.241'
+        USER='vdoftp'
+        PASS='vdoftp$$12!'
+	LOCAL='/users/$USER'
+        TARGETFOLDER='VDOPIA_SDK'
+        lftp -e "
+        open $HOST
+        user $USER $PASS
+        lcd $LOCAL
+        mirror  $TARGETFOLDER
+        bye
+        "
+        x=`echo $?`
+        if [ $x == 0 ]; then
+        echo "$TARGETFOLDER copied to $LOCAL successfully"
+        ls -l $LOCAL
+        exit 0
+        else
+        echo "Please try again"
+        exit 1
+        fi
+}
+
+if [ -f /usr/local/bin/lftp ]; then
+	echo "lftp is already installed"
+	cd /users/$USER
+	connect_remote_FTP
+	exit 0
+else
+	echo "lftp is not installed on your system...Installing it now...Please wait..."
+	cd /users/$USER
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew doctor
+	mkdir  ~/.lftp
+	touch ~/.lftp/rc
+	echo "set ssl:verify-certificate no" >> ~/.lftp/rc
+	echo "set ssl:check-hostname no" >> ~/.lftp/rc
+	echo "set ftp:ssl-force false" >> ~/.lftp/rc
+	echo "set ftp:ssl-protect-list false" >> ~/.lftp/rc
+	echo "set ftp:ssl-allow false" >> ~/.lftp/rc
+	brew install lftp
+	sleep 2;
+	echo "lftp is now installed..."
+	connect_remote_FTP
+	exit 0
+fi     
+
+
+
